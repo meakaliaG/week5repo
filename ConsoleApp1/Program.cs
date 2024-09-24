d1f7c5d40a9dd5469ba6db1157432edb3dc720b6
meakaliaG <124390595+meakaliaG@users.noreply.github.com>
2024-09-24 09:09:21 -0400

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int Math = 0;
+            Math += 6;
+            Console.WriteLine(Math);
+        }
+    }
+}
